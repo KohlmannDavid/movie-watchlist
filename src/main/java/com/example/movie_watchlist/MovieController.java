c0061547619e4fab6d8d80a58c63c6cef08322c2
David Kohlmann <david.kohlmann@student.htw-berlin.de>
2026-04-24 10:17:12 +0200

    Milestone1

--- src/main/java/com/example/movie_watchlist/MovieController.java
+++ src/main/java/com/example/movie_watchlist/MovieController.java
@@ -12,7 +12,7 @@ public class MovieController {
         return List.of(
                 new Movie(1L, "Inception", "Christopher Nolan", 2010, "Sci-Fi", 9, true, "Mindbending!"),
                 new Movie(2L, "Interstellar", "Christopher Nolan", 2014, "Sci-Fi", 10, true, "Absolute masterpiece"),
-                new Movie(3L, "The Dark Knight", "Christopher Nolan", 2008, "Action", 10, true, "Best superhero movie ever")
+                new Movie(3L, "The Dark Knight 2", "Christopher Nolan", 2008, "Action", 10, true, "Best superhero movie ever")
         );
     }
 }
