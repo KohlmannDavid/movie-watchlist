9e53352ffa307b9a5a16706ee1fe5be16085a62c
David Kohlmann <david.kohlmann@student.htw-berlin.de>
2026-07-04 13:53:38 +0200

    add backend tests

--- src/test/java/com/example/movie_watchlist/MovieWatchlistApplicationTests.java
+++ src/test/java/com/example/movie_watchlist/MovieWatchlistApplicationTests.java
@@ -10,4 +10,11 @@ class MovieWatchlistApplicationTests {
 	void contextLoads() {
 	}
 
+	@Test
+	void movieEntityCreation() {
+		Movie movie = new Movie(1L, "Inception", "Christopher Nolan", 2010, "Sci-Fi", 9, true, "Great movie");
+		assert movie.getTitle().equals("Inception");
+		assert movie.getDirector().equals("Christopher Nolan");
+		assert movie.getReleaseYear() == 2010;
+	}
 }
